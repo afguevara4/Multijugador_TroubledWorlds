9c66d37cc677e6540801b75afc00e118a87b1c01
Denniss2C <dscaisa@espe.edu.ec>
2023-01-17 23:59:33 -0500

    Tools

---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+    public static int coinCount = 0;
+
+    private void Start()
+    {
+        Tools.coinCount = Tools.coinCount + 1;
+        Debug.Log("Empieza el juego" + Tools.coinCount + "tools");
+    }
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.CompareTag("Player") == true)
+        {
+
+        }
+        Tools.coinCount--;
+        Debug.Log("recogida de moneda" + Tools.coinCount + "tools");
+
+        if (Tools.coinCount == 0)
+        {
+            Debug.Log("Pasar al siguiente Nivel");
+        }
+        Destroy(gameObject);
+    }
+}
