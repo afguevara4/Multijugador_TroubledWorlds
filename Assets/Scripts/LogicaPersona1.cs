8a2eda57121493572e23150e39b0fba58ec2926e
afguevara4 <77500643+afguevara4@users.noreply.github.com>
2023-01-18 01:53:17 -0500

    salto

---
+++ Assets/Scripts/LogicaPersona1.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersona1 : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float fuerzaSalto = 8f;
+    public bool puedoSaltar;
+
+    #region Private Fields
+    [SerializeField]
+    public float velocidadMovimiento = 5.0f;
+    public float velocidadRotacion = 200.0f;
+    public Animator animator;
+    public float x, y;
+
+    #endregion
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        puedoSaltar = false;
+        animator = GetComponent<Animator>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+
+    }
+
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+       
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("salte", true);
+                rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+            }
+            animator.SetBool("tocoSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+    }
+    #endregion
+
+    public void EstoyCayendo()
+    {
+        animator.SetBool("tocoSuelo", false);
+        animator.SetBool("salte", false);
+    }
+}
--- Assets/Scripts/PlayerAnimatorManager.cs
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -6,6 +6,9 @@ namespace Com.AdrianG.BattleKnights
 {
     public class PlayerAnimatorManager : MonoBehaviourPun
     {
+        public Rigidbody rb;
+        public float fuerzaSalto = 8f;
+        public bool pudoSaltar;
 
         #region Private Fields
         [SerializeField]
@@ -13,17 +16,22 @@ namespace Com.AdrianG.BattleKnights
         public float velocidadRotacion = 200.0f;
         public Animator animator;
         public float x, y;
+
+
         #endregion
         #region MonoBehaviour Callbacks
 
         void Start()
         {
+            pudoSaltar = false;
             animator = GetComponent<Animator>();
             if (!animator)
             {
                 Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
             }
         }
+
+        
 
         void Update()
         {
@@ -35,6 +43,21 @@ namespace Com.AdrianG.BattleKnights
             {
                 return;
             }
+
+            if (pudoSaltar)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    animator.SetBool("salte", true);
+                    rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+                }
+                animator.SetBool("tocoSuelo", true);
+            }
+            else
+            {
+                EstoyCayendo();
+            }
+
             x = Input.GetAxis("Horizontal");
             y = Input.GetAxis("Vertical");
 
@@ -43,9 +66,16 @@ namespace Com.AdrianG.BattleKnights
 
             animator.SetFloat("VelX", x);
             animator.SetFloat("VelY", y);
-
         }
         #endregion
+
+        public void EstoyCayendo()
+        {
+            animator.SetBool("tocoSuelo", false);
+            animator.SetBool("salte", false);
+        }
+
+        
 
     }
 }
